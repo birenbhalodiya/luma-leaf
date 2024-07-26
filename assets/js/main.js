be4c59b3ce5f6594bb1ccab3b3a9d05a25ba89bc
birenbhalodiya <birenbhalodiya19@gmail.com>
2024-07-26 20:39:37 +0530

    code pushed

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Navbar Mobile Screen Menu ================================
+
+function add() {
+  let nav = document.querySelector("ul");
+  nav.classList.add("active");
+}
+function remove() {
+  let nav = document.querySelector("ul");
+  nav.classList.remove("active");
+}
+
+// Navbar Mobile Screen Menu ================================
