5342a034c4115acbc48e55e33d456471bc266ea8
birenbhalodiya <birenbhalodiya19@gmail.com>
2024-07-29 14:41:43 +0530

    responsive header design

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,18 @@
 function add() {
   let nav = document.querySelector("ul");
   nav.classList.add("active");
+  let mobileMenu = document.querySelector(".mobile_menu");
+  mobileMenu.classList.add("click");
+  let closeMenu = document.querySelector(".close_menu");
+  closeMenu.classList.remove("click");
 }
 function remove() {
   let nav = document.querySelector("ul");
   nav.classList.remove("active");
+  let mobileMenu = document.querySelector(".mobile_menu");
+  mobileMenu.classList.remove("click");
+  let closeMenu = document.querySelector(".close_menu");
+  closeMenu.classList.add("click");
 }
 
 // Navbar Mobile Screen Menu ================================
